5b8309d401b5fe2767ab51706c4089df5b2cee58
Eugene Lazutkin <eugene@lazutkin.com>
2013-04-20 01:48:28 -0500

    Split dom (main) and domutils.

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ define(["heya-has/sniff", "./window"], function(has, win){
 	};
 
 	if(has("ie")){
-		module.byId = function(id, doc){
+		module.byId = function byId(id, doc){
 			if(typeof id != "string"){
 				return id;
 			}
@@ -47,7 +47,7 @@ define(["heya-has/sniff", "./window"], function(has, win){
 			}
 		};
 	}else{
-		module.byId = function(id, doc){
+		module.byId = function byId(id, doc){
 			// inline'd type check.
 			// be sure to return null per documentation, to match IE branch.
 			return (typeof id == "string" ? (doc || win.doc).getElementById(id) : id) || null; // DOMNode
@@ -85,7 +85,7 @@ define(["heya-has/sniff", "./window"], function(has, win){
 	 };
 	 =====*/
 
-	module.isDescendant = function(/*DOMNode|String*/ node, /*DOMNode|String*/ ancestor){
+	module.isDescendant = function isDescendant(/*DOMNode|String*/ node, /*DOMNode|String*/ ancestor){
 		// summary:
 		//		Returns true if node is a descendant of ancestor
 		// node: DOMNode|String
@@ -101,83 +101,13 @@ define(["heya-has/sniff", "./window"], function(has, win){
 			node = module.byId(node);
 			ancestor = module.byId(ancestor);
 			while(node){
-				if(node == ancestor){
+				if(node === ancestor){
 					return true; // Boolean
 				}
 				node = node.parentNode;
 			}
 		}catch(e){ /* squelch, return false */ }
 		return false; // Boolean
-	};
-
-
-	// TODO: do we need setSelectable in the base?
-
-	// Add feature test for user-select CSS property
-	// (currently known to work in all but IE < 10 and Opera)
-	has.add("css-user-select", function(global, doc, element){
-		// Avoid exception when dom.js is loaded in non-browser environments
-		if(!element){ return false; }
-
-		var style = element.style;
-		var prefixes = ["Khtml", "O", "ms", "Moz", "Webkit"],
-			i = prefixes.length,
-			name = "userSelect",
-			prefix;
-
-		// Iterate prefixes from most to least likely
-		do{
-			if(typeof style[name] !== "undefined"){
-				// Supported; return property name
-				return name;
-			}
-		}while(i-- && (name = prefixes[i] + "UserSelect"));
-
-		// Not supported if we didn't return before now
-		return false;
-	});
-
-	/*=====
-	module.setSelectable = function(node, selectable){
-		// summary:
-		//		Enable or disable selection on a node
-		// node: DOMNode|String
-		//		id or reference to node
-		// selectable: Boolean
-		//		state to put the node in. false indicates unselectable, true
-		//		allows selection.
-		// example:
-		//		Make the node id="bar" unselectable
-		//	|	dojo.setSelectable("bar");
-		// example:
-		//		Make the node id="bar" selectable
-		//	|	dojo.setSelectable("bar", true);
-	};
-	=====*/
-
-	var cssUserSelect = has("css-user-select");
-	module.setSelectable = cssUserSelect ? function(node, selectable){
-		// css-user-select returns a (possibly vendor-prefixed) CSS property name
-		module.byId(node).style[cssUserSelect] = selectable ? "" : "none";
-	} : function(node, selectable){
-		node = module.byId(node);
-
-		// (IE < 10 / Opera) Fall back to setting/removing the
-		// unselectable attribute on the element and all its children
-		var nodes = node.getElementsByTagName("*"),
-			i = nodes.length;
-
-		if(selectable){
-			node.removeAttribute("unselectable");
-			while(i--){
-				nodes[i].removeAttribute("unselectable");
-			}
-		}else{
-			node.setAttribute("unselectable", "on");
-			while(i--){
-				nodes[i].setAttribute("unselectable", "on");
-			}
-		}
 	};
 
 	return module;
