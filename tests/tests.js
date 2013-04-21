b50908ebebc7f0ae2b21d153f4b43617582cc731
Eugene Lazutkin <eugene@lazutkin.com>
2013-04-21 02:57:29 -0500

    Added tests for the "construct" family.

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,10 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
 (["module", "heya-unit", "../window", "../main", "../class", "../style", "../prop",
-	"../construct", "../geometry", "../attr", "../winutils", "../domutils"],
+	"../destroy", "../place", "../toDom", "../construct",
+	"../geometry", "../attr", "../winutils", "../domutils"],
 function(module, unit, win, dom, cls, style, prop,
-	construct, geom, attr, winutils, domutils){
+	destroy, place, toDom, ctr,
+	geom, attr, winutils, domutils){
 	"use strict";
 
 	// tests
@@ -158,6 +160,116 @@ function(module, unit, win, dom, cls, style, prop,
 			eval(t.TEST("style.get(elem, 'display') === 'block'"));
 			eval(t.TEST("style.get(elem, 'visibility') === 'visible'"));
 			prop.set(elem, "innerHTML", "<span>Hello, world!</span>");
+			eval(t.TEST("elem.firstChild"));
+			eval(t.TEST("elem.firstChild === elem.lastChild"));
+			eval(t.TEST("elem.firstChild.tagName.toLowerCase() === 'span'"));
+			eval(t.TEST("elem.firstChild.firstChild"));
+			eval(t.TEST("elem.firstChild.firstChild.data === 'Hello, world!'"));
+			win.body().removeChild(elem);
+		},
+		// destroy
+		function test_destroy(t){
+			var elem = win.doc.createElement("p");
+			elem.id = "abc";
+			elem.innerHTML = "<span id='def'>Hello, world!</span>";
+			win.body().appendChild(elem);
+			eval(t.TEST("dom.byId('abc')"));
+			eval(t.TEST("dom.byId('def')"));
+			destroy.empty(elem);
+			eval(t.TEST("dom.byId('abc')"));
+			eval(t.TEST("!dom.byId('def')"));
+			eval(t.TEST("elem.innerHTML === ''"));
+			elem.innerHTML = "<span id='def'>Hello, world!</span>";
+			eval(t.TEST("dom.byId('abc')"));
+			eval(t.TEST("dom.byId('def')"));
+			destroy(elem);
+			eval(t.TEST("!dom.byId('abc')"));
+			eval(t.TEST("!dom.byId('def')"));
+		},
+		// place
+		function test_place(t){
+			// preparing a setup
+			destroy.empty(win.body());
+			var elem = win.doc.createElement("p");
+			elem.id = "a";
+			elem.innerHTML = "<span id='a1'>Hello, world!</span>";
+			win.body().appendChild(elem);
+			elem = win.doc.createElement("p");
+			elem.id = "b";
+			elem.innerHTML = "<span id='b1'>Hello, world!</span>";
+			win.body().appendChild(elem);
+			elem = win.doc.createElement("p");
+			elem.id = "c";
+			elem.innerHTML = "<span id='c1'>Hello, world!</span>";
+			win.body().appendChild(elem);
+			// making inserts
+			place("<p id='x1'></p>", "b", "before");
+			place("<p id='x2'></p>", "b", "after");
+			place("<p id='x3'></p>", "c", "replace");
+			place("<span id='x4'></span>", "a", "first");
+			place("<span id='x5'></span>", "a", "last");
+			place("<span id='x6'></span>", "a");
+			place("<span id='x7'></span>", "b", "only");
+			place("<span id='x8'></span>", "b", 0);
+			place("<span id='x9'></span>", "b", 2);
+			// checking top-level nodes
+			var ids = [];
+			for(var node = win.body().firstChild; node; node = node.nextSibling){
+				ids.push(node.id);
+			}
+			eval(t.TEST("t.unify(ids, ['a', 'x1', 'b', 'x2', 'x3'])"));
+			// checking "a"
+			ids = [];
+			for(node = dom.byId("a").firstChild; node; node = node.nextSibling){
+				ids.push(node.id);
+			}
+			eval(t.TEST("t.unify(ids, ['x4', 'a1', 'x5', 'x6'])"));
+			// checking "b"
+			ids = [];
+			for(node = dom.byId("b").firstChild; node; node = node.nextSibling){
+				ids.push(node.id);
+			}
+			eval(t.TEST("t.unify(ids, ['x8', 'x7', 'x9'])"));
+			destroy.empty(win.body());
+		},
+		// toDom
+		function test_toDom(t){
+			var frag = toDom("<p>Hello!</p>");
+			eval(t.TEST("frag.nodeType === 1 && frag.tagName.toLowerCase() === 'p'"));
+			frag = toDom("<span>Hello!</span>");
+			eval(t.TEST("frag.nodeType === 1 && frag.tagName.toLowerCase() === 'span'"));
+			frag = toDom("<option>Hello!</option>");
+			eval(t.TEST("frag.nodeType === 1 && frag.tagName.toLowerCase() === 'option'"));
+			frag = toDom("<tr><td>1</td></tr>");
+			eval(t.TEST("frag.nodeType === 1 && frag.tagName.toLowerCase() === 'tr'"));
+			frag = toDom("<td>1</td>");
+			eval(t.TEST("frag.nodeType === 1 && frag.tagName.toLowerCase() === 'td'"));
+			frag = toDom("<th>1</th>");
+			eval(t.TEST("frag.nodeType === 1 && frag.tagName.toLowerCase() === 'th'"));
+			frag = toDom("<tr><td>1</td></tr><tr><td>2</td></tr>");
+			eval(t.TEST("frag.nodeType === 11"));
+			eval(t.TEST("frag.childNodes.length === 2"));
+			eval(t.TEST("frag.firstChild.tagName.toLowerCase() === 'tr'"));
+			eval(t.TEST("frag.lastChild.tagName.toLowerCase() === 'tr'"));
+			frag = toDom("Hello, world!");
+			eval(t.TEST("frag.nodeType === 3"));
+			eval(t.TEST("frag.data === 'Hello, world!'"));
+		},
+		// ctr
+		function test_ctr(t){
+			var elem = ctr("p", {
+					id: "abc",
+					className: "a b c",
+					style: {
+						display: "block",
+						visibility: "visible"
+					},
+					innerHTML: "<span>Hello, world!</span>"
+				}, win.body());
+			eval(t.TEST("elem.id === 'abc'"));
+			eval(t.TEST("elem.className === 'a b c'"));
+			eval(t.TEST("style.get(elem, 'display') === 'block'"));
+			eval(t.TEST("style.get(elem, 'visibility') === 'visible'"));
 			eval(t.TEST("elem.firstChild"));
 			eval(t.TEST("elem.firstChild === elem.lastChild"));
 			eval(t.TEST("elem.firstChild.tagName.toLowerCase() === 'span'"));
