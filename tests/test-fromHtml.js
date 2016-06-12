0898fee6cfb782ae973971472ab4901209919994
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2016-06-11 19:57:25 -0500

    Better text test.

--- tests/test-fromHtml.js
+++ tests/test-fromHtml.js
@@ -3,9 +3,12 @@ define(["module", "heya-unit", "../fromHtml"], function (module, unit, fromHtml)
 
 	unit.add(module, [
 		function test_text (t) {
-			var node = fromHtml('<p>Hello!</p>');
+			var node = fromHtml('Hello!');
 			eval(t.TEST('node.nodeName == "#document-fragment"'));
 			eval(t.TEST('node.childNodes.length == 1'));
+			var div = document.createElement('div');
+			div.appendChild(node);
+			eval(t.TEST('div.innerHTML === "Hello!"'));
 		}
 	]);
 
